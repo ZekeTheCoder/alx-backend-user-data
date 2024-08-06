fb936aefc02b1b7b0fb19794fa449f900b158a76
Thabang Maphangisane <thabangh647@gmail.com>
2024-08-06 17:39:30 +0200

    6. Basic auth

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -8,7 +8,7 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 from api.v1.views import app_views
 from api.v1.auth.auth import Auth
-
+from api.v1.auth.basic_auth import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -17,26 +17,10 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 
 auth_type = os.getenv('AUTH_TYPE')
-if auth_type == 'auth':
+if auth_type == 'basic_auth':
+    auth = BasicAuth()  # instance of BasicAuth
+else:
     auth = Auth()
-
-
-@app.errorhandler(401)
-def unauthorized(error) -> str:
-    """Handle Unauthorized error (401)."""
-    return jsonify({"error": "Unauthorized"}), 401
-
-
-@app.errorhandler(403)
-def forbidden(error) -> str:
-    """Handle Forbidden error (403)."""
-    return jsonify({"error": "Forbidden"}), 403
-
-
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Handle Not found error (404).   """
-    return jsonify({"error": "Not found"}), 404
 
 
 @app.before_request
@@ -61,6 +45,24 @@ def before_request() -> str:
     # Current User Check
     if (auth.current_user(request)) is None:
         abort(403)
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """Handle Unauthorized error (401)."""
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """Handle Forbidden error (403)."""
+    return jsonify({"error": "Forbidden"}), 403
+
+
+@app.errorhandler(404)
+def not_found(error) -> str:
+    """ Handle Not found error (404).   """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
