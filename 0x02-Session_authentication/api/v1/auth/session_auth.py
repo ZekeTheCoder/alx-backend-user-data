d02736254654931a869a67c741dab5c2e73894e9
Thabang Maphangisane <thabangh647@gmail.com>
2024-08-07 15:10:21 +0200

    3. User ID for Session ID

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -28,3 +28,17 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Retrieve the User ID associated with a Session ID.
+
+        Args:
+            session_id (str): The session ID to look up.
+
+        Returns:
+            str: The associated User ID if session ID is valid, otherwise None.
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+
+        return self.user_id_by_session_id.get(session_id)
