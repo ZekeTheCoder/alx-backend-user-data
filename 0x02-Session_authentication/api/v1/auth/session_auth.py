79137837cf6c9fa18f435951d6c9e379d3aa82b3
Thabang Maphangisane <thabangh647@gmail.com>
2024-08-08 12:44:15 +0200

    6. Use Session ID for identifying a User

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-""" Session Authentication Module """
+""" Session-based Authentication Module """
 
 import uuid
+from os import getenv
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -31,10 +33,8 @@ class SessionAuth(Auth):
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """Retrieve the User ID associated with a Session ID.
-
         Args:
             session_id (str): The session ID to look up.
-
         Returns:
             str: The associated User ID if session ID is valid, otherwise None.
         """
@@ -42,3 +42,38 @@ class SessionAuth(Auth):
             return None
 
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """Retrieve a User instance based on the session cookie value.
+        Args:
+            request: The request object to extract the session cookie from.
+        Returns:
+            User: User instance associated with the session ID if found, otherwise
+        """
+        # Get the session ID from the request's cookies
+        session_id = self.session_cookie(request)
+
+        # Get the user ID associated with the session ID
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+
+        # Fetch and return the User instance from the database
+        return User.get(user_id)
+
+    def session_cookie(self, request=None):
+        """Retrieve the session cookie value from the request.
+        Args:
+            request: The request object to extract the cookie from.
+        Returns:
+            str: value of the session cookie if it exists, otherwise None.
+        """
+        if request is None:
+            return None
+
+        # Get the cookie value from SESSION_NAME environment variable
+        cookie_name = getenv('SESSION_NAME')
+        if not cookie_name:
+            return None
+
+        return request.cookies.get(cookie_name)
