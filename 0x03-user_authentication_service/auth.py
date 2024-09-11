2b286b41d104d22793b94ba24b6bac2a5e2fa2dd
Thabang Maphangisane <thabangh647@gmail.com>
2024-09-11 13:27:06 +0200

    8. Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -38,7 +38,7 @@ class Auth:
         """ Validate the login credentials and return boolean value """
         try:
             user = self._db.find_user_by(email=email)
-            return bcrypt.checkpw(password.encode('utf-8'),
-                                  user.hashed_password)
         except NoResultFound:
             return False
+        return bcrypt.checkpw(password.encode('utf-8'),
+                              user.hashed_password)
